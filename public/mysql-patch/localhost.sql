9e1438a3e1cfcc5e0a21fc0e1a26bd7996868631
shafiqul <shafiqruet@gmail.com>
2017-05-27 16:23:22 +0600

    update sal code

--- public/mysql-patch/localhost.sql
+++ public/mysql-patch/localhost.sql
@@ -5,8 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `homestead` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `homestead`;
 
 DROP TABLE IF EXISTS `employees`;
 CREATE TABLE `employees` (
