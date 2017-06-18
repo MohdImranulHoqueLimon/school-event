8c5cec32b9ecb5c3ee8b66d385f2756dd8e3833d
ImranulHoqueLimon <limon1036@hotmail.com>
2017-06-19 02:48:23 +0600

    make events table migration

---
+++ database/migrations/2017_06_19_024045_create_table_events.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('description');
+            $table->integer('amount');
+            $table->boolean('fixed_amount')->default(true);
+            $table->integer('guest_amount');
+            $table->boolean('guest_allow')->default(false);
+            $table->integer('created_by')->unsigned()->default(0);
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
