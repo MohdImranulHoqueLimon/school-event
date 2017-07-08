9d1bbd4132dbd93938ccab26e104ab8fa9d58139
ImranulHoqueLimon <limon1036@hotmail.com>
2017-07-08 16:22:56 +0600

    Update checking users status

--- resources/views/user/shared/sidebar.blade.php
+++ resources/views/user/shared/sidebar.blade.php
@@ -1,15 +1,6 @@
 <div class="page-sidebar-wrapper">
-    <!-- BEGIN SIDEBAR -->
-    <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-    <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
     <div class="page-sidebar navbar-collapse collapse">
-        <!-- BEGIN SIDEBAR MENU -->
-        <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
-        <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
-        <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
-        <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-        <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
-        <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+
         <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true"
             data-slide-speed="200" style="padding-top: 20px">
             <!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
@@ -19,8 +10,7 @@
                     <span></span>
                 </div>
             </li>
-            <!-- END SIDEBAR TOGGLER BUTTON -->
-            <!-- DOC: To remove the search box from the sidebar you just need to completely remove the below "sidebar-search-wrapper" LI element -->
+
             <li class="nav-item @if (Route::is('profile.*'))start active open @endif">
                 <a href="{{url('/user/profile')}}" class="nav-link ">
                     <i class="icon-home"></i>
@@ -31,6 +21,7 @@
                 </a>
             </li>
 
+            @if(\Illuminate\Support\Facades\Auth::user()->status == 1)
             <li class="nav-item @if (Route::is('students.*'))start active open @endif">
                 <a href="{{url('/user/students')}}" class="nav-link ">
                     <i class="fa fa-graduation-cap"></i>
@@ -40,6 +31,7 @@
                     @endif
                 </a>
             </li>
+            @endif
 
             <li class="nav-item @if (Route::is('payments.*'))start active open @endif">
                 <a href="{{url('/user/payments')}}" class="nav-link ">
