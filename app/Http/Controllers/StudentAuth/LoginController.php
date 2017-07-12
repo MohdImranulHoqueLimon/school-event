ea73cfe7dc2ad581ac0840fa91932103247bf08b
limon103 <limon1036@hotmail.com>
2017-07-13 00:51:10 +0600

    fix namespace

--- app/Http/Controllers/StudentAuth/LoginController.php
+++ app/Http/Controllers/StudentAuth/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\StudentAuth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Traits\AuthenticatesUsers;
--- app/Http/Controllers/StudentAuth/RegisterController.php
+++ app/Http/Controllers/StudentAuth/RegisterController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\StudentAuth;
 
 use App\Models\Student;
 use App\User;
